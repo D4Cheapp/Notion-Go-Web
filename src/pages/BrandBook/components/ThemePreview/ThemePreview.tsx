bd30231c450db308abcaae556584d0a7747feb5f
D4Cheap <scherbakov720@gmail.com>
2024-05-02 02:25:20 +0300

    Fix vercel deploying

--- src/pages/BrandBook/components/ThemePreview/ThemePreview.tsx
+++ src/pages/BrandBook/components/ThemePreview/ThemePreview.tsx
@@ -2,25 +2,24 @@ import React from 'react';
 import TodoSection from './TodoSection';
 import s from './ThemePreview.module.scss';
 
-const darkTheme = {
-  background: '#34342f',
-  secondBackground: '#42413c',
-  textColor: '#fbf5e5',
-  secondText: '#97948a',
-  green: '#8ec07c',
-  theme: 'dark' as 'light' | 'dark',
-};
-
-const lightTheme = {
-  background: '#fbf5e5',
-  secondBackground: '#eee8d9',
-  textColor: '#333',
-  secondText: '#83817b',
-  green: '#92cb7d',
-  theme: 'light' as 'light' | 'dark',
-};
-
 const ThemePreview = (): React.ReactNode => {
+  const darkTheme = {
+    background: '#34342f',
+    secondBackground: '#42413c',
+    textColor: '#fbf5e5',
+    secondText: '#97948a',
+    green: '#8ec07c',
+    theme: 'dark' as 'light' | 'dark',
+  };
+  const lightTheme = {
+    background: '#fbf5e5',
+    secondBackground: '#eee8d9',
+    textColor: '#333',
+    secondText: '#83817b',
+    green: '#92cb7d',
+    theme: 'light' as 'light' | 'dark',
+  };
+
   return (
     <section className={s.root}>
       <div className={s.theme}>
